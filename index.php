872ac3b1899db28a9af1fd6a2fba1790aec8cb96
drobin04 <90974129+drobin04@users.noreply.github.com>
2022-11-06 06:37:16 -0500

    More formatting and segmentation

--- index.php
+++ index.php
@@ -1,27 +1,6 @@
-<!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head >
-    <title>Dashboardify</title>
-	<link  type="text/css" rel="stylesheet" href="index.css">
+<!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml"><head><title>Dashboardify</title><link type="text/css" rel="stylesheet" href="index.css">
     <style>  
-            <?php //Grab custom CSS from Database - This part is important for background colors for example
-            	// Create and connect to SQLite database file.
-				$db_file = new PDO('sqlite:Dashboardify.s3db');
-				// Prepare SELECT statement.
-				$select = "SELECT * FROM UserCSS";
-				$stmt = $db_file->prepare($select);
-				
-				// Execute statement.
-				$stmt->execute();
-				
-				// Get the results.
-				$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-				
-				foreach($results as $row) {
-                    			echo $row["CSS"];
-				}
-            ?>
+            <?php include("usercss.php"); //Load user-defined CSS for page from DB. Moved to file to make this file easier to read, as this code has long been finished. ?>
     </style>
     <?php include("logoutredirect.php");?> 
 </head>
@@ -32,7 +11,6 @@
 		<?php   
 			$querystring = $_SERVER['QUERY_STRING']; //Get value from URL
 			$WidgetID = str_replace("EditRecID=","",$querystring); //Strip out extra junk
-
 			If ($WidgetID <> "") {
 				$db_file = new PDO('sqlite:Dashboardify.s3db'); // Connect to SQLite database file.
 				$select = "SELECT * FROM Widgets"; // Prepare SELECT statement.
@@ -52,12 +30,8 @@
 						$WidgetNotes = $row["Notes"];
 						}
 				}
-			} else {
-				// Prepare unused variables
-				$WidgetTypeValue = "Bookmark";$WidgetURLValue = "";$WidgetDisplayText = "";$WidgetPositionX = "";$WidgetPositionY = "";$WidgetSizeX = "";$WidgetSizeY = "";$WidgetCSSClass = "";$WidgetNotes = "";$WidgetID = "";
-			}
+			} else {$WidgetTypeValue = "Bookmark";$WidgetURLValue = "";$WidgetDisplayText = "";$WidgetPositionX = "";$WidgetPositionY = "";$WidgetSizeX = "";$WidgetSizeY = "";$WidgetCSSClass = "";$WidgetNotes = "";$WidgetID = "";} // Prepare unused variables
 		?>
-			
                     <button type="button" style="float: left !important;" onclick="document.getElementById('light').style.display='none';document.getElementById('fade').style.display='none'">Close</button>
                     <br />
                     <div id="columnc" class="column" style="width: 85% !important; clear: both; margin: 0 auto;">
@@ -89,25 +63,20 @@
                         <button id="btnSubmitNewWidget">Submit</button>
                     </div>
 		</div>
-
 		<!--Edit CSS Box-->
         <div id="light2" class="white_content" style="right: initial !important; left:0 !important; width:400px !important;">
-			<button type="button" style="float: left !important;" onclick="document.getElementById('light2').style.display='none';">Close</button>
-			<br />
+			<button type="button" style="float: left !important;" onclick="document.getElementById('light2').style.display='none';">Close</button><br />
 			<div id="columnd" class="column" style="width: 85% !important; height:100% !important; clear: both; margin: 0 auto;">
 				<header >CSS<hr /></header>
 				<button ID="btnUpdateCSS"  Text="Save CSS" /><br />
 				<input ID="txtCSS"  TextMode="MultiLine" style="width:100%; height:80%;"></input>
-				
 			</div>
 		</div>
-
         <div>
 			<!--Buttons at top left-->
             <button type="button" style="float: left !important;" onclick="document.getElementById('light').style.display='block';document.getElementById('fade').style.display='block'">New Widget</button>
             <button type="button" style="float: left !important;" onclick="document.getElementById('light2').style.display='block';">Edit CSS</button>
             <button type="button" style="float: left !important;" onclick="var all = document.getElementsByClassName('editbuttons'); for (var i = 0; i < all.length; i++) {all[i].style.display = 'initial';}">Edit Widgets</button><br />
-
 			<?php //Check for dashboards for user; Create first dashboard if none exist, then load any widgets found for dashboard if exists.
 				include("shared_functions.php");
 				$db_file = new PDO('sqlite:Dashboardify.s3db'); // Connect to SQLite database file.
@@ -119,36 +88,20 @@
 				$dashboards = selectquery("Select RecID From Dashboards Where UserID = '" . $userid . "'");
 				$dashboardid = "";
 				Try {
-
 					if (count($dashboards) >= 1) {
-						//echo "DASHBOARDS FOUND: " . (count($dashboards));
-						debuglog($dashboards, "Dashboards Array");
-						$dashboardid = $dashboards[0]["RecID"];
-						debuglog($dashboardid, "Selected Dashboard ID.");
+						debuglog($dashboards, "Dashboards Array"); $dashboardid = $dashboards[0]["RecID"]; debuglog($dashboardid, "Selected Dashboard ID.");
 					} else { // If dash not found, create one
-						//echo "NO DASHBOARDS FOUND.";
 						$dashboardid = GUID(); 
 						$sql1 = "INSERT INTO Dashboards (DashboardID, UserID) VALUES ('" . $dashboardid . "', '" . $userid . "')";
-						debuglog($sql1,"SQL Dashboard Insert Query");
-						execquery($sql1);
+						debuglog($sql1,"SQL Dashboard Insert Query"); execquery($sql1);
 					} 
-				} catch (exception $ex) {
-						echo $ex;
-						debuglog($ex,"Exception found during dashboard checks");
-				}
-				
-				// Load Widgets For Selected Dashboard
+				} catch (exception $ex) {echo $ex;debuglog($ex,"Exception found during dashboard checks");
+				} // Load Widgets For Selected Dashboard
 				$select = "SELECT * FROM Widgets Where DashboardRecID = '" . $dashboardid . "'";
-				$stmt = $db_file->prepare($select);
-				$stmt->execute();
+				$stmt = $db_file->prepare($select); $stmt->execute();
 				$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-				
-				//Variable for site url
-				include("siteurlconfig.php");
-
-				foreach($results as $row) {
-
-					//Re-useable texts
+				include("siteurlconfig.php"); //Variable for site url
+				foreach($results as $row) { //Starting with Re-useable texts
 					$editbuttonscss = "<a class='editbuttons' style='display:none;height:24px; width:24px;' href='";
 					$imgstylecss = "<img style='height:24px; width:24px;' src='";
 					$PositionAndCSSClass = "left: " . $row["PositionX"] . "px; top: " . $row["PositionY"] . "px; width: " . $row["SizeX"] . "px; height: " . $row["SizeY"] . "px; max-width: " . $row["SizeX"] . "px;' class='" . $row["WidgetCSSClass"] . "'>";
@@ -160,7 +113,6 @@
 						$sqlquery = $row["sqlquery"];
 						debuglog($sqlquery,"sql query about to be executed");
 						$result = "";
-						
 						try { // reference for sql query from PHP - https://social.technet.microsoft.com/wiki/contents/articles/1258.accessing-sql-server-databases-from-php.aspx
 							$connectionInfo = array( "Database"=>$sqldbname, "UID"=>$sqlserveruser, "PWD"=>$sqlserverpass);
 							//debuglog($connectionInfo,"debug - connection info");
@@ -178,46 +130,18 @@
 					   catch (Exception $ex) {
 							debuglog($ex, "error during SQL server connection");
 					   }
-
 						echo "<div style='padding: 5px; margin: 5px; width:100px; background-color: lightgrey;  border: 1px solid black;' class='" . $row["WidgetCSSClass"] . "'>
-						<a target='_blank' href='". $row["WidgetURL"] ."'>". $row["BookmarkDisplayText"] . ": " . $result ."</a>
-						
-						" . $editbuttonscss . $siteurl . "?EditRecID=" . $row["RecID"] . "'>
-							" . $imgstylecss . $siteurl . "icons/edit.png'></img></a>
-						" . $editbuttonscss . $siteurl . "DeleteWidget.php?RecID=" . $row["RecID"] . "'>
-							" . $imgstylecss . $siteurl . "icons/cancel.png'></img>
-						</a>
-					</div>";
-					}
-
-					If ($row["WidgetType"] == "Bookmark") {
-						echo "<div style='padding: 5px; margin: 5px; width:100px; background-color: lightgrey;  border: 1px solid black;' class='" . $row["WidgetCSSClass"] . "'>
-						<a target='_blank' href='". $row["WidgetURL"] ."'>". $row["BookmarkDisplayText"] ."</a>
-						" . $editbuttonscss . $siteurl . "?EditRecID=" . $row["RecID"] . "'>
-							" . $imgstylecss . $siteurl . "icons/edit.png'></img>
-						</a>		
-						" . $editbuttonscss . $siteurl . "DeleteWidget.php?RecID=" . $row["RecID"] . "'>
-							" . $imgstylecss . $siteurl . "icons/cancel.png'></img>
+						<a target='_blank' href='". $row["WidgetURL"] ."'>". $row["BookmarkDisplayText"] . ": " . $result ."</a>" . $editbuttonscss . $siteurl . "?EditRecID=" . $row["RecID"] . "'>" . $imgstylecss . $siteurl . "icons/edit.png'></img></a>" . $editbuttonscss . $siteurl . "DeleteWidget.php?RecID=" . $row["RecID"] . "'>" . $imgstylecss . $siteurl . "icons/cancel.png'></img>
 						</a></div>";
-					};
+					}
+					If ($row["WidgetType"] == "Bookmark") {echo "<div style='padding: 5px; margin: 5px; width:100px; background-color: lightgrey;  border: 1px solid black;' class='" . $row["WidgetCSSClass"] . "'><a target='_blank' href='". $row["WidgetURL"] ."'>". $row["BookmarkDisplayText"] ."</a>" . $editbuttonscss . $siteurl . "?EditRecID=" . $row["RecID"] . "'>" . $imgstylecss . $siteurl . "icons/edit.png'></img></a>" . $editbuttonscss . $siteurl . "DeleteWidget.php?RecID=" . $row["RecID"] . "'>" . $imgstylecss . $siteurl . "icons/cancel.png'></img></a></div>";};
 					$combined = "<div style='margin:15px; position:absolute; background-color: white;  border: 1px solid black;" . $PositionAndCSSClass . $editbuttonscss . $siteurl . "?EditRecID=" . $row["RecID"] . "'>" . $imgstylecss . $siteurl . "icons/edit.png'></img></a>" . $editbuttonscss . $siteurl . "DeleteWidget.php?RecID=" . $row["RecID"] . "'>" . $imgstylecss . $siteurl . "icons/cancel.png'></img></a>";
-					If ($row["WidgetType"] == "IFrame") {
-						echo $combined . "<iframe style='height:100%;width:100%' src='". $row["WidgetURL"] ."'></iframe></a></div>";
-					}
-					If ($row["WidgetType"] == "Notes") {
-						echo $combined . "<p>". $row["Notes"] ."</p></div>";
-					}
-					If ($row["WidgetType"] == "HTMLEmbed") {
-						echo $combined . $row["Notes"] ."</div>";
-					}
-					
-				}
-				echo "</table>";
+					If ($row["WidgetType"] == "IFrame") {echo $combined . "<iframe style='height:100%;width:100%' src='". $row["WidgetURL"] ."'></iframe></a></div>";}
+					If ($row["WidgetType"] == "Notes") {echo $combined . "<p>". $row["Notes"] ."</p></div>";}
+					If ($row["WidgetType"] == "HTMLEmbed") {echo $combined . $row["Notes"] ."</div>";}} echo "</table>";
 			?>
         </div>
     </form>
-    <script type="text/javascript" src="index.js"></script>
-	<br />
-	<a href="logout.php">Log Out</a>
+    <script type="text/javascript" src="index.js"></script><br /><a href="logout.php">Log Out</a>
 </body>
 </html>
---
+++ usercss.php
@@ -0,0 +1,17 @@
+<?php //Grab custom CSS from Database - This part is important for background colors for example
+            	// Create and connect to SQLite database file.
+				$db_file = new PDO('sqlite:Dashboardify.s3db');
+				// Prepare SELECT statement.
+				$select = "SELECT * FROM UserCSS";
+				$stmt = $db_file->prepare($select);
+				
+				// Execute statement.
+				$stmt->execute();
+				
+				// Get the results.
+				$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+				
+				foreach($results as $row) {
+                    			echo $row["CSS"];
+				}
+            ?>
