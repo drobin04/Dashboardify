1f51eaf7fefa92df86263c08f8b506f4998f99ec
drobin04 <90974129+drobin04@users.noreply.github.com>
2023-12-22 21:43:53 -0800

    Compartmentalizing / Organizing code for index page a bit

---
+++ actions/index/Widget_Handling.php
@@ -0,0 +1,129 @@
+<?php
+
+    $editbuttonscss = "<a class='editbuttons' style='display:none;height:24px; width:24px;' href='";
+    $imgstylecss = "<img style='height:24px; width:24px;' src='";
+    $PositionAndCSSClass = "left: " . $row["PositionX"] . "px; top: " . $row["PositionY"] . "px; width: " . $row["SizeX"] . "px; height: " . $row["SizeY"] . "px; width: " . $row["SizeX"] . "px;' class='widget resize " . $row["WidgetCSSClass"] . "'>";
+    $combined = "<div id='" . $row["RecID"] . "' style='margin:15px; position:absolute; background-color: white;  border: 1px solid black;" . $PositionAndCSSClass . $editbuttonscss . $siteurl . "?EditRecID=" . $row["RecID"] . "&SelectDashboardID=" . $dashboardid . "'>" . $imgstylecss . $siteurl . "icons/edit.png'></img></a>" . $editbuttonscss . $siteurl . "actions/DeleteWidget.php?RecID=" . $row["RecID"] . "'>" . $imgstylecss . $siteurl . "icons/cancel.png'></img></a>";
+    $floatingbookmarkPositionAndCSSClass = "left: " . $row["PositionX"] . "px; top: " . $row["PositionY"] . "px;' class='widget resize " . $row["WidgetCSSClass"] . "'>";
+    $floatingbookmark = "<div id='" . $row["RecID"] . "' class='widget bookmark' style='position: absolute; width:100px; background-color: lightgrey;  border: 1px solid black; " 
+    . $floatingbookmarkPositionAndCSSClass . $editbuttonscss 
+    . $siteurl . "?EditRecID=" . $row["RecID"] . "&SelectDashboardID=" 
+    . $dashboardid . "'>" . $imgstylecss . $siteurl . "icons/edit.png'></img></a>" . $editbuttonscss 
+    . $siteurl . "actions/DeleteWidget.php?RecID=" . $row["RecID"] . "'>" . $imgstylecss . $siteurl . "icons/cancel.png'></img></a>";
+    
+    If ($row["WidgetType"] == "SQLServerScalarQuery") {
+        $sqlservaddress = $row["sqlserveraddress"];
+        $sqldbname = $row["sqldbname"];
+        $sqlserveruser = $row["sqluser"];
+        $sqlserverpass = $row["sqlpass"];
+        $sqlquery = $row["sqlquery"];
+        debuglog($sqlquery,"sql query about to be executed");
+        $result = "";
+        try { // reference for sql query from PHP - https://social.technet.microsoft.com/wiki/contents/articles/1258.accessing-sql-server-databases-from-php.aspx
+            $connectionInfo = array( "Database"=>$sqldbname, "UID"=>$sqlserveruser, "PWD"=>$sqlserverpass);
+            //debuglog($connectionInfo,"debug - connection info");
+            $conn = sqlsrv_connect( $sqlservaddress, $connectionInfo);
+            if( $conn ) {
+                    debuglog("Connection established.");
+                $tempresult = sqlsrv_query($conn,$sqlquery);
+                if ( sqlsrv_fetch( $tempresult ) )
+                    {$result = sqlsrv_get_field( $tempresult, 0);debuglog($result, "Results of SQL query");}
+            }else{debuglog("Connection could not be established."); debuglog(sqlsrv_errors());}
+        }
+        catch (Exception $ex) {
+            debuglog($ex, "error during SQL server connection");
+        }
+        echo $combined . "<p>" . $row["BookmarkDisplayText"] . ": " . $result ."</p></div>";
+    }
+    elseIf (($row["WidgetType"] == "Bookmark") and ($row["PositionX"] != 0 and $row["PositionX"] != "")) { // For Bookmarks with custom positions
+        echo $floatingbookmark 
+        ." <div style='padding: 5px; width: 100%; class='widget bookmark "
+        . $row["WidgetCSSClass"] 
+        . "'><a target='_blank' href='" 
+        . $row["WidgetURL"] 
+        . "'>" 
+        . $row["BookmarkDisplayText"] 
+        ."</a></div></div>";}
+    elseIf ($row["WidgetType"] == "Bookmark" and ($row["PositionX"] == 0 or $row["PositionX"] == "")) {echo "<div id='" . $row["RecID"] . "' style='padding: 5px; margin: 5px; width:100px; background-color: lightgrey;  border: 1px solid black;' class='widget bookmark" . $row["WidgetCSSClass"] . "'><a target='_blank' href='". $row["WidgetURL"] ."'>". $row["BookmarkDisplayText"] ."</a>" . $editbuttonscss . $siteurl . "?EditRecID=" . $row["RecID"] . "&SelectDashboardID=" . $dashboardid . "'>" . $imgstylecss . $siteurl . "icons/edit.png'></img></a>" . $editbuttonscss . $siteurl . "actions/DeleteWidget.php?RecID=" . $row["RecID"] . "'>" . $imgstylecss . $siteurl . "icons/cancel.png'></img></a></div>";}
+    elseIf ($row["WidgetType"] == "IFrame") {echo $combined . "<iframe style='height:100%;width:100%' src='". $row["WidgetURL"] ."'></iframe></a></div>";}
+    elseIf ($row["WidgetType"] == "Collapseable IFrame") {
+        $combined2 = "<div id='" . $row["RecID"] . "' style='display:none; position:absolute; background-color: white;  border: 1px solid black;" . "width: " . $row["SizeX"] . "px; height: " . $row["SizeY"] . "px; width: " . $row["SizeX"] . "px;' class='widget resize " . $row["WidgetCSSClass"] . "'>" . $editbuttonscss . $siteurl . "?EditRecID=" . $row["RecID"] . "&SelectDashboardID=" . $dashboardid . "'>" . $imgstylecss . $siteurl . "icons/edit.png'></img></a>" . $editbuttonscss . $siteurl . "actions/DeleteWidget.php?RecID=" . $row["RecID"] . "'>" . $imgstylecss . $siteurl . "icons/cancel.png'></img></a>";
+    
+        $hidden = "<div id='' class='collapse' style='" . "left: " . $row["PositionX"] . "px; top: " . $row["PositionY"] . "px; width: " . $row["SizeX"] . "px; height: 20px; width: " . $row["SizeX"] . "px;'" . "'>";
+        echo $hidden . "<a style='border: none !important;' class='collapse' onclick='opencollapsediframe(&quot;" . $row["RecID"] . "&quot;)'>" . $row["BookmarkDisplayText"] . "</a>";
+        echo $combined2 . "<iframe style='height:100%;width:100%;' id='" . $row["RecID"] . "/iframe' src2='". $row["WidgetURL"] ."'></iframe></a></div>";
+        echo "</div>"; //this wraps combined variable, into a surrounding div.
+    }
+    elseIf ($row["WidgetType"] == "Notes") {echo $combined . "<p class='note' style='padding-left: 15px; padding-right: 15px;'><md-block>". $row["Notes"] ."</md-block></p></div>";}
+    elseIf ($row["WidgetType"] == "HTMLEmbed") {echo $combined . $row["Notes"] ."</div>";}
+    elseIf ($row["WidgetType"] == "SQLiteResultsList") {
+        // Wrap in a try statement!! Throw the error into debug + the widget. 
+        try {
+        // Get variables
+        If ($row["sqlquery"] == "") {$row["sqlquery"] = "Select 'Query Not Present' as Error";}
+        $sqlitedbname = $row["sqldbname"];
+        $sqlitequery = $row["sqlquery"];
+        debuglog($sqlitequery,"about to execute query");
+        $dbpath = 'sqlite:' . $sqlitedbname; // setup proper sqlite DSN pathname from supplied input
+        $db_file = new PDO($dbpath);
+        $stmt9 = $db_file->prepare($sqlitequery);
+        $stmt9->execute();
+        $localresults = $stmt9->fetchAll(PDO::FETCH_ASSOC);
+        debuglog($localresults,"Query results");
+        echo $combined . generateTableFromObjects($localresults) ."</div>";
+        } catch (exception $e) {
+            
+        } finally {
+        }
+    } // End of SQLite DB 'If' block
+    elseIf ($row["WidgetType"] == "Javascript API Request") {
+        
+    
+        // End of Widget Loading for-each list
+    } else { // Look for and handle custom widget providers defined in DB!
+                // WidgetProviderName
+                // CSS_Styling
+                // HTML_Content
+                // PHP_To_Run
+                // CREATE TABLE CustomWidgetProviders (
+                // WidgetProviderName TEXT,
+                // CSS_Styling TEXT  NULL,
+                // HTML_Content TEXT  NULL,
+                // PHP_To_Run TEXT  NULL,
+            // OK... PHP eval handler test... lets go...
+            // At this point we have a 'widget' defined that didn't match any other pre-defined widget
+            // 	Types... So we'll see if there's a matching widget handler with this name in the DB.
+            
+            
+            // THE FOLLOWING CODE USES EVAL() WHICH IS DANGEROUS.
+            // ONLY ALLOW RUNNING THE PHP PART OF THIS IF USER IS ADMIN?
+            $w_provider = $row["WidgetType"];
+            $prov = selectquery("Select WidgetProviderName, CSS_Styling, HTML_Content, PHP_To_Run From 
+            CustomWidgetProviders Where WidgetProviderName = '" . $w_provider . "'");
+            if (isset($prov)) {
+                debuglog($prov, "Custom Widget Provider");
+                $p = $prov[0];
+                $widget_css = $p["CSS_Styling"];
+                $w_HTML_Content = $p["HTML_Content"];
+                $w_PHP_To_Run = $p["PHP_To_Run"];
+                
+                
+                // Draw the widget first i guess?
+                echo $combined;
+                if (getAuthMode() == "None") {
+                    // Bad. 
+                }
+                if (AmIAdmin()) {
+                eval($w_PHP_To_Run);
+                } // ONLY RUN EVAL IF I AM ADMIN.
+                echo $w_HTML_Content ."</div>";
+
+//https://enplnnfh4ifmp.x.pipedream.net/
+            }
+
+
+}
+
+
+
+?>
---
+++ actions/index/load_and_prep_dashboard.php
@@ -0,0 +1,126 @@
+<?php
+
+//debuglog("shared functions module loaded.");
+$db_file = new PDO('sqlite:Dashboardify.s3db'); // Connect to SQLite database file.
+$sessionid = $_COOKIE["SessionID"]; debuglog($sessionid, "SessionID"); //Get User for Session ID
+$userid = selectquery("Select UserID From Sessions Where SessionID = '" . $sessionid . "'")[0]["UserID"]; debuglog($userid, "User ID found for user");
+if (isadmin($userid)) { // Display Setup button if user is an admin. 
+    echo "<button type='button' style='float:left !important;''><a class='nodeco' href='setup.php'>Setup</a></button>";
+}
+echo "<script>localStorage.setItem('userID', '$userid');</script>";
+$dashboards = selectquery("Select * From Dashboards Where UserID = '" . $userid . "'"); // Query for dashboards for user. 
+$dashboardname = "";
+$dashboardid = "";
+$embeddable = "";
+$dashboardphotourl = ""; // Used later at end of script to pre-populate value into input for CSS box.
+Try { // Load Dashboard list
+    $count = count($dashboards);
+    debuglog($count, "Count variable");
+    if ((int)$count == 1) {
+        foreach($dashboards as $row) {
+            debuglog($dashboards, "Dashboards Array - Single Result"); 
+            $dashboardid = $dashboards[0]["DashboardID"]; 
+            debuglog($dashboardid, "Selected Dashboard ID."); 
+            $dashboardphotourl = $row["BackgroundPhotoURL"]; 
+            $usercss = $row["CustomCSS"];
+            $dashboardname = $row["Name"];
+            $embeddable = $row["Embeddable"];
+        }
+    } elseif ((int)$count > 1) {
+        debuglog($dashboards, "Dashboards Array - Multiple Results"); 
+        echo "<label> Dashboard: </label><select ID='ddlSelectedDashboard' name='SelectedDashboard' onchange='loadselecteddashboard()'>";
+        $match = 0;
+        $matcheddashboardid = -1;
+        If (Isset($_GET["SelectDashboardID"])) { $matcheddashboardid = $_GET["SelectDashboardID"];}
+
+        foreach($dashboards as $row) {
+            $recid = $row["DashboardID"];
+            if ($row["DefaultDB"] == "Y") {
+                $dashboardid = $recid;
+                $dashboardname = $row["Name"];
+                $embeddable = $row["Embeddable"];
+                $dashboardphotourl = $row["BackgroundPhotoURL"];
+                $usercss = $row["CustomCSS"];
+                debuglog($dashboardid, "Selected Dashboard ID.");
+                
+                echo "<option value='" . $row["DashboardID"] . "'";
+                If ($recid == $matcheddashboardid){echo "selected='selected'";} elseif ($matcheddashboardid <> -1) {} else {echo "selected='selected'";}
+                echo ">" . $row["Name"] . "</option>";
+
+            } else {
+                echo "<option value='" . $row["DashboardID"] . "'";
+                If ($recid == $matcheddashboardid){echo "selected='selected'";}
+                echo ">" . $row["Name"] . "</option>";
+
+            }
+            if (isset($_GET["SelectDashboardID"])) {
+                if ($row["DashboardID"] == $_GET["SelectDashboardID"]) { 
+                    $match = 1; 
+                    debuglog(array($match,$matcheddashboardid), "Match / selected db from URL if present?"); 
+                    $dashboardphotourl = $row["BackgroundPhotoURL"];
+                    $usercss = $row["CustomCSS"];
+                
+                }
+            }
+        }
+        echo "</select><br />";
+
+        //debuglog($_GET['SelectDashboardID'], "Selected Dashboard ID from URL");
+        If ($match == 1) {$dashboardid = $_GET["SelectDashboardID"];}
+
+    } else { // If dash not found, create one
+        $dashboardid = GUID(); 
+        $defaultdashimage = "";
+        if (file_exists('config/defaultdashboardurl.txt')) {
+            $defaultdashimage = file_get_contents('config/defaultdashboardurl.txt');
+            $dashboardphotourl = $defaultdashimage;
+        }
+        $sql1 = "INSERT INTO Dashboards (DashboardID, UserID, DefaultDB, Name, BackgroundPhotoURL) VALUES ('" . $dashboardid . "', '" . $userid 
+        . "', 'Y', 'Main', '" . $defaultdashimage . "')";
+        debuglog($sql1,"SQL Dashboard Insert Query"); 
+        execquery($sql1); //Create Dashboard
+        $dashboardname = "Main";
+        $embeddable = "";
+        // First / Newly Created Dashboard; Populate Global/Default Widgets
+
+        // Query for Global widgets
+        $globalwidgets = selectquery("Select * From Widgets Where Global = '1'");
+        debuglog($globalwidgets, "Global Widgets List");
+        foreach($globalwidgets as $w) {
+            
+            //Prepare Variables
+            $sep = "','"; // Seperator
+            $sqlserveraddress = $w["sqlserveraddress"];
+            $sqldbname = $w["sqldbname"];
+            $sqlusername = $w["sqluser"];
+            $sqlpass = $w["sqlpass"];
+            $sqlquery = $w["sqlquery"];
+            $globaldefault = "0";
+            
+                                
+            // Prepare INSERT statement.
+            $select = "INSERT INTO Widgets (WidgetType,BookmarkDisplayText,PositionX,PositionY,SizeX,SizeY,WidgetURL,WidgetCSSClass,Notes,DashboardRecID
+            ,sqlserveraddress,sqldbname,sqluser,sqlpass,sqlquery, Global) 
+            VALUES 
+            ('" 
+            . $w["WidgetType"] . $sep . $w["BookmarkDisplayText"] . $sep . $w["PositionX"] 
+            . $sep . $w["PositionY"] . $sep . $w["SizeX"] . $sep . $w["SizeY"] . $sep . $w["WidgetURL"] 
+            . $sep . $w["WidgetCSSClass"] . $sep . str_replace("'", "''",$w["Notes"]) . $sep . $dashboardid . $sep 
+            .$sqlserveraddress . $sep . $sqldbname . $sep . $sqlusername . $sep . $sqlpass . $sep . $sqlquery .
+            $sep . $globaldefault . "')";
+            debuglog($select, "Query for inserting global widget");
+            execquery($select);
+            //header('Location: index.php');
+            echo "<script>location.reload();</script>";
+        }
+
+    } 
+} catch (exception $ex) {echo $ex;debuglog($ex,"Exception found during dashboard checks");
+    debuglog($ex); echo $ex;
+} 
+//Try to set background photo and CSS for dashboard
+If (isset($dashboardphotourl)) {echo "<style>body {background-image: url('" . $dashboardphotourl . "'); background-size: cover;} " . $usercss . "</style>";}
+
+
+
+?>
