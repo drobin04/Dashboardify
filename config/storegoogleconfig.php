9668102319bde6878f251dd91ab1a9de05aa0fb2
drobin04 <dougrobinson3491@gmail.com>
2024-01-04 12:15:28 -0500

    Update storegoogleconfig.php

--- config/storegoogleconfig.php
+++ config/storegoogleconfig.php
@@ -21,16 +21,16 @@ $code = $params['code'];
 echo $code;
 
 // Exchange the authorization code for an access token
-$client_id = '814465180043-ir2l2aejp965j0eug05kfi51clid8f7a.apps.googleusercontent.com';
-$client_secret = file_get_contents("google_config_app_secret.txt");
-$redirect_uri = 'https://dashboardify.app/config/storegoogleconfig.php';
+$clientid = '814465180043-ir2l2aejp965j0eug05kfi51clid8f7a.apps.googleusercontent.com';
+$clientsecret = file_get_contents("google_config_app_secret.txt");
+$redirecturi = 'https://dashboardify.app/config/storegoogleconfig.php';
 
-$token_url = 'https://oauth2.googleapis.com/token';
+$tokenurl = 'https://oauth2.googleapis.com/token';
 $data = array(
     'code' => $code,
-    'client_id' => $client_id,
-    'client_secret' => $client_secret,
-    'redirect_uri' => $redirect_uri,
+    'client_id' => $clientid,
+    'client_secret' => $clientsecret,
+    'redirect_uri' => $redirecturi,
     'grant_type' => 'authorization_code'
 );
 
@@ -43,7 +43,7 @@ $options = array(
 );
 
 $context = stream_context_create($options);
-$response = file_get_contents($token_url, false, $context);
+$response = file_get_contents($tokenurl, false, $context);
 $token_data = json_decode($response, true);
 
 // Use the access token to make API requests
