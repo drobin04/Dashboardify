37633eebad7a6f02f4de277c8647ea7a3fe98557
drobin04 <90974129+drobin04@users.noreply.github.com>
2024-01-21 01:33:32 -0500

    Widget Handling PHP Notes Widget Bugfixes

--- actions/index/Widget_Handling.php
+++ actions/index/Widget_Handling.php
@@ -112,8 +112,12 @@ break;
 case "Notes":
 	// Escape the Notes value to replace '' with '
 	$notesvalue = str_replace("''", "'", $row["Notes"]);
-	$combined_notes = "<div class='notes' id='" . $row["RecID"] . "' style='margin:15px; position:absolute; background-color: white;  border: 1px solid black;" . $PositionAndCSSClass . $editbuttonscss . $siteurl . "?EditRecID=" . $row["RecID"] . "&SelectDashboardID=" . $dashboardid . "'>" . $imgstylecss . $siteurl . "icons/edit.png'></img></a>" . $deletebuttoncss . ">" . $imgstylecss . $siteurl . "icons/cancel.png'></img></a>";
-$floatingbookmarkPositionAndCSSClass = "left: " . $row["PositionX"] . "px; top: " . $row["PositionY"] . "px;' class='widget resize " . $row["WidgetCSSClass"] . "'>";
+	$left = $row['PositionX'] . "px";
+	$top = $row["PositionY"] . "px";
+	$width = $row["SizeX"] . "px";
+	$height = $row["SizeY"] . "px";
+	$combined_notes = "<div class='notes resize' id='" . $row["RecID"] . "' style='margin:15px; position:absolute; background-color: white;  border: 1px solid black;left: $left; top: $top; width: $width; height: $height;'>" . "<a class='editbuttons' style='display:none;height:24px; width:24px;' href='" . $siteurl . "?EditRecID=" . $row["RecID"] . "&SelectDashboardID=" . $dashboardid . "'>" . $imgstylecss . $siteurl . "icons/edit.png'></img></a>" . $deletebuttoncss . ">" . $imgstylecss . $siteurl . "icons/cancel.png'></img></a>";
+
 	echo $combined_notes . "<div style='height: 100%;' id='" . $row['RecID'] . "_note'><p class='note' style='padding-left: 15px; padding-right: 15px;'><md-block>". $notesvalue ."</md-block></p></div></div>";
 	break;
 
